944878558b3668c0373f2d8ead598ac38d68400d
Tygo de Vries <zttdevries@gmail.com>
2026-07-30 15:28:18 +0200

    WIP Updated file

--- club/index.js
+++ club/index.js
@@ -1,13 +1,40 @@
-function download(name)
-{
-    var element = document.createElement('a');
-    element.setAttribute('href', name);
-    element.setAttribute('download', name.split('/').pop());
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-}
+async function loadContent() {
+    const response = await fetch('./sessions.json');
+    const json = await response.json();
+    const content = document.getElementById('content');
 
-function redirect(url) {
-    window.open(url, '_blank');
+    let html = '';
+
+    for (const year of json.years) {
+        html += `
+            <div class="year-container">
+                <div class="year-title">${year.title}</div>
+                <div class="sessions-container">`;
+
+        for (const session of year.sessions) {
+
+            var todayClass = "";
+
+            if(session.today == true)
+            {
+                todayClass = "today";
+            }
+
+            html += `
+                <div class="session">
+                    <img src="${session.img}" class="session-img" alt="The session ${session.title}">
+                    <div class="session-content ${todayClass}">
+                        <div class="session-title">${session.title}</div>
+                        <div class="session-discr">${session.about}</div>
+                    </div>
+                </div>`;
+        }
+
+        html += `
+                </div>
+            </div>`;
+    }
+
+    content.innerHTML = html; // Set innerHTML only once at the end
 }
+loadContent();
--- r/package/index.js
+++
@@ -1,9 +0,0 @@
-// A function that will download a file when called from a spesific path (r/package/example.unitypackage)
-function downloadFile() {
-    var element = document.createElement('a');
-    element.setAttribute('href', '/r/package/example.unitypackage');
-    element.setAttribute('download', 'example.unitypackage');
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-}
